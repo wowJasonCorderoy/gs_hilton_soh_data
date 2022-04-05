657d0774fcffd2743460d73d863af5fbf927f412
Jason Corderoy <jcorderoy@woolworths.com.au>
2022-04-05 12:12:53 +1000

    req fixed. filename param in func.

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from google.cloud import bigquery
 from datetime import datetime,timezone  
 import os
 import hashlib
+import numpy as np
     
 #### Declare constants
 N_COLUMNS = 7
@@ -45,12 +46,12 @@ def get_bucket_name(event: dict = None) -> str:
 def save_to_bucket_name(bucketname: str) -> str:
     return bucketname + "_output"
 
-def is_correctFileType(fileName: str = None, regex: str = r".*htm[l]?$") -> bool:
+def is_correctFileType(fileName: str = None, regex: str = r".*xls[x]?$") -> bool:
     """[summary]
 
     Args:
         fileName (str, optional): name of file. Defaults to None.
-        regex (str, optional): regex expression to evaluate. Defaults to r".*htm[l]?$".
+        regex (str, optional): regex expression to evaluate. Defaults to r".*xls[x]?$".
 
     Returns:
         bool: if True then regex matched fileName else False
@@ -92,6 +93,67 @@ def copy_blob(
             destination_bucket.name,
         )
     )
+    
+def infer_site(filename: str):
+    if 'trug' in filename.lower():
+        return 'Truganina'
+    if 'hw' in filename.lower():
+        return 'Heathwood'
+    if 'bun' in filename.lower():
+        return 'Bunbury'
+    if 'offsite storage' in filename.lower(): # e.g. filename 'Offsite Storage 28032022' is actually Heathwood
+        return 'heathwood'
+    else:
+        return 'Other'
+
+def infer_table_id(filename: str):
+    if 'offsite' in filename.lower():
+        return 'hilton.hilton_offsitestorage'
+    if "po's" in filename.lower():
+        return 'hilton.hilton_offsitestorage'
+    else:
+        return 'hilton.hilton_inventory'
+
+def load_site_soh(params_save_dest: str):
+    headers = ['ACTUALINVENTORYLEVELEND', 'DATE', 'DESCRIPTION', 'PRODUCTID', 'STOCKINGPOINTID', 'MANUFACTUREDDATE']
+    dtypes = {'ACTUALINVENTORYLEVELEND': np.float64,
+        'DESCRIPTION': 'str', 
+        'PRODUCTID': 'str', 
+        'STOCKINGPOINTID': 'str',
+            }
+    parse_dates = ['DATE', 'MANUFACTUREDDATE']        
+    df = pd.read_excel(params_save_dest, names=headers, dtype=dtypes, parse_dates=parse_dates, usecols="A:F")
+    return df
+
+def load_offsite_soh(params_save_dest: str):
+    headers = ['VENDOR', 'NAME_1', 'WOW_PO_NUMBER', 'GOODS_SUPPLIER', 'GOODS_SUPPLIER_NAME', 'PURCH_DOC', 'PLANT', 'MATERIAL', 'MATERIAL_DESCRIPTION',
+                'MATL_GROUP', 'DOC_DATE', 'DELIV_DATE', 'SCHEDULED_QTY', 'OUN1', 'OUN2', 'QTY_DELIVERED']
+    dtypes = {'VENDOR': 'str',
+                'NAME_1': 'str',
+                'WOW_PO_NUMBER': 'str',
+                'GOODS_SUPPLIER': 'str',
+                'GOODS_SUPPLIER_NAME': 'str',
+                'PURCH_DOC': 'str',
+                'PLANT': 'str',
+                'MATERIAL': 'str',
+                'MATERIAL_DESCRIPTION': 'str',
+                'MATL_GROUP': 'str',
+                'SCHEDULED_QTY': np.float64,
+                'OUN1': 'str',
+                'OUN2': 'str',
+                'QTY_DELIVERED': np.float64,
+            }
+    parse_dates = ['DOC_DATE','DELIV_DATE']        
+    df = pd.read_excel(params_save_dest, names=headers, dtype=dtypes, parse_dates=parse_dates, usecols="A:P")
+    return df
+
+def get_function_2_load_data(filename: str):
+    if 'offsite' in filename.lower():
+        return load_offsite_soh
+    if "po's" in filename.lower():
+        return load_offsite_soh
+    else:
+        return load_site_soh
 
 def run(event, context):
     """Triggered by a change to a Cloud Storage bucket.
@@ -121,98 +183,40 @@ def run(event, context):
         client = storage.Client()
         bucket = client.get_bucket(bucketName)
         params_blob = bucket.get_blob(fileName)
-        params_save_dest = "/tmp/data_file.html"
+        params_save_dest = "/tmp/data_file.xlsx"
         params_blob.download_to_filename(params_save_dest)
         print(f"{fileName} saved to {params_save_dest}")
     except:
         print(f"{fileName} does not exist. ABORTING.")
         return
     
-    # find each block of data
-    re_per_block = re.compile(">([^< ]+)<")
-
-    textfile = open(params_save_dest, 'r')
-    filetext = textfile.read()
-    textfile.close()
-    matches = re.findall(re_per_block, filetext)
-    
+    # hash time
     file_contents_md5 = hashlib.md5(open(params_save_dest,'rb').read()).hexdigest()
-
-    # 1st element is the report heading
-    # next are table headings
-    # then rest is the data
-
-    # first replace &nbsp; with space
-    replace_these = {'&nbsp;': ' ',
-                    '&#38;': '&',
-                    }
-    matches_clean = matches
-    for k, v in replace_these.items():
-        matches_clean = [x.replace(k, v) for x in matches_clean]
-    matches_clean = [re.sub(r'\s{2,}', " ", x).strip() for x in matches_clean]
-
-    # make a list of lists then dataframe
-    # sometimes 1st column name is 2nd item in list (in which case report title is first) other times it's 1st.
-    if matches_clean[0] != 'Material':
-        report_name = matches_clean.pop(0)
-    else:
-        report_name = 'Stocklist with remainging SLED and days from pack'
-    lol = list(zip(*[iter(matches_clean)]*N_COLUMNS))
-    # remove duplicate headers
-    df_headings = lol.pop(0)
-    lol = [x for x in lol if x != df_headings]
-    # to dataframe
-    df = pd.DataFrame(lol, columns=df_headings)
-
-    # types
-    df_schema = {
-        'Material': lambda x: x.astype('str'),
-        'WoW material no.': lambda x: x.astype('str'),
-        'Material description': lambda x: x.astype('str'),
-        'Batch': lambda x: x.astype('str'),
-        'Unrestricted': lambda x: [int(re.sub(r'[^0-9]', "", str(s))) for s in x],
-        'BUn': lambda x: x.astype('str'),
-        'Manuf. Dte' : lambda x: pd.to_datetime(x, dayfirst=True),
-    }
-
-    for k,v in df_schema.items():
-        df[k] = df_schema[k](df[k])
-
+    
+    # read xlsx as pandas dd    
+    df = get_function_2_load_data(fileName)(params_save_dest=params_save_dest)
+    
     # add an as at column
+    df['site'] = infer_site(fileName)
     df['upload_utc_dt'] = now_utc
     df['filename'] = fileName
     df['file_contents_md5'] = file_contents_md5
-    
-    # clean headings for BQ save
-    colname_map = {
-        'Material': 'material',
-        'WoW material no.': 'wow_material_no',
-        'Material description': 'material_description',
-        'Batch': 'batch',
-        'Unrestricted': 'unrestricted',
-        'BUn': 'bun',
-        'Manuf. Dte' : 'manufacture_date',
-    }
-    bq_df = df.rename(columns=colname_map)
-    
+      
     # save to cloud storage
     saveFileName = now_utc.strftime("%Y%m%d_%H:%M:%S")+"_"+fileName
     saveLocation = "gs://" + save_to_bucketname + "/" + saveFileName
-    bq_df.to_csv(saveLocation+'.csv', index=False)
-    bq_df.to_pickle(saveLocation+'.pk')
+    df.to_csv(saveLocation+'.csv', index=False)
+    df.to_pickle(saveLocation+'.pk')
 
-    #clean_saveFileName = re.sub(r'[^0-9a-zA-Z.]','_', saveFileName) #copy_blob has issues with spaces and special chars so replace them
-    #copy_blob(bucket_name=bucketName, blob_name=fileName, destination_bucket_name=save_to_bucketname, destination_blob_name=clean_saveFileName)
     copy_blob(bucket_name=bucketName, blob_name=fileName, destination_bucket_name=save_to_bucketname, destination_blob_name=saveFileName)
 
     # save to BQ
     client = bigquery.Client(project=PROJECT_ID)
-    table_id = 'masterdata_view.hilton_soh_daily'
+    table_id = infer_table_id(fileName)
 
     job_config = bigquery.LoadJobConfig(write_disposition="WRITE_APPEND")
 
-    job = client.load_table_from_dataframe(bq_df, table_id, job_config=job_config)
-
+    job = client.load_table_from_dataframe(df, table_id, job_config=job_config)
 
 if __name__ == "__main__":
     run()
